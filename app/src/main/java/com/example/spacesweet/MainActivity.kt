abb0570fc8ecad35cdb9561f448983d3a2d52db3
Itzel Ramon <itzelramonf@gmail.com>
2023-04-10 16:36:16 -0500

    Se inicializo LoginViewModel en el Activity

--- app/src/main/java/com/example/spacesweet/MainActivity.kt
+++ app/src/main/java/com/example/spacesweet/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.spacesweet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -22,6 +23,8 @@ import com.example.spacesweet.ui.register.RegisterScreen
 import com.example.spacesweet.ui.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
+    private val loginViewModel: LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -62,7 +65,7 @@ class MainActivity : ComponentActivity() {
                                 buttonTitle = "Get Start",
                             )
                         }
-                        composable("login") { LoginScreen(LoginViewModel(), navController) }
+                        composable("login") { LoginScreen(loginViewModel, navController) }
                         composable("register") {RegisterScreen(navController)}
                         composable("welcome") { WelcomeScreen(navController)}
                         composable("bottomNavigate"){ BottomNavigation()}
