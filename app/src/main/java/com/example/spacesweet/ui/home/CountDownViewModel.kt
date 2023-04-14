a5ed0501899360dc0175d42a803ec63a91482f3a
azulcolor <lealpastelin@gmail.com>
2023-04-14 11:25:25 -0500

    Home logic

---
+++ app/src/main/java/com/example/spacesweet/ui/home/CountDownViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.spacesweet.ui.home
+
+import android.os.CountDownTimer
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.spacesweet.ui.home.TimeFormat.timeFormat
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+
+class CountDownViewModel: ViewModel() {
+    private var countDownTimer: CountDownTimer? = null
+
+    private val userInputHour = TimeUnit.HOURS.toMillis(0)
+    private val userInputMinute = TimeUnit.MINUTES.toMillis(0)
+    private val userInputSecond = TimeUnit.SECONDS.toMillis(5)
+
+    val initialTotalTimeInMillis = userInputHour + userInputMinute + userInputSecond
+    var timeLeft = mutableStateOf(initialTotalTimeInMillis)
+    val countDownInterval = 1000L // 1 seconds is the lowest
+
+    val timerText = mutableStateOf(timeLeft.value.timeFormat())
+
+    val hasFinished = mutableStateOf(false)
+
+    fun startCountDownTimer() = viewModelScope.launch {
+        countDownTimer = object : CountDownTimer(timeLeft.value, countDownInterval) {
+            override fun onTick(currentTimeLeft: Long) {
+                timerText.value = currentTimeLeft.timeFormat()
+                timeLeft.value = currentTimeLeft
+            }
+
+            override fun onFinish() {
+                timerText.value = initialTotalTimeInMillis.timeFormat()
+                hasFinished.value = true
+                timerText.value = initialTotalTimeInMillis.timeFormat()
+                timeLeft.value = initialTotalTimeInMillis
+                countDownTimer?.cancel()
+            }
+        }.start()
+    }
+
+}
---
+++ app/src/main/java/com/example/spacesweet/ui/home/TimeFormat.kt
@@ -0,0 +1,14 @@
+package com.example.spacesweet.ui.home
+
+import java.util.concurrent.TimeUnit
+
+object TimeFormat {
+    private const val FORMAT = "%02d:%02d:%02d"
+
+    fun Long.timeFormat(): String = String.format(
+        FORMAT,
+        TimeUnit.MILLISECONDS.toHours(this),
+        TimeUnit.MILLISECONDS.toMinutes(this) % 60,
+        TimeUnit.MILLISECONDS.toSeconds(this) % 60
+    )
+}
